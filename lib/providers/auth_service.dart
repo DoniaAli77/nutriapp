31ce7aa046b342d6b47c6e95b9a442935a6c4a5a
DoniaAli77 <donia.ali.7799@gmail.com>
2024-07-19 15:44:57 +0300

    v111

--- lib/providers/auth_service.dart
+++ lib/providers/auth_service.dart
@@ -26,6 +26,7 @@ class AuthService {
 
   Future<UserModel?> registerWithEmailPassword(String email, String password) async {
     try {
+     
       UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       return _userFromFirebase(result.user);
     } catch (e) {
